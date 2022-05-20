8569bb6ca68a5a4d635dd37bb4c4b6f714f3aa63
Kaustubh <84623873+kaustubh1126@users.noreply.github.com>
2022-05-20 13:26:47 -0700

    ignore node modules

--- BackEnd/src/main/java/project/demo/employee/Repo/IngredientRepo.java
+++ BackEnd/src/main/java/project/demo/employee/Repo/IngredientRepo.java
@@ -1,15 +1,15 @@
 package project.demo.employee.Repo;
+
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
 import org.springframework.data.repository.query.Param;
 
 import project.demo.employee.Model.ingredient;
 
-
-public interface IngredientRepo extends JpaRepository<ingredient, Long> 
-{
+public interface IngredientRepo extends JpaRepository<ingredient, Long> {
     @Query(value = "SELECT * FROM ingredient ing where ing.recipeid = :recid", nativeQuery = true)
-    List<ingredient> findIngById(@Param("recid")Long recid);
+    List<ingredient> findIngById(@Param("recid") Long recid);
 }
